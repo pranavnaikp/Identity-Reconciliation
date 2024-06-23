cf1e4f6bcd036b14c98be25108378bf4b9509cbb
pranavnaikp <pranavnaik250901@gmail.com>
2024-06-23 13:07:20 +0530

    chore: add additional functionality to get the details

--- src/routes/identify.ts
+++ src/routes/identify.ts
@@ -79,5 +79,57 @@ export default (connection: Connection) => {
     }
   });
 
+  router.get('/contactByEmail/:email', async (req: Request, res: Response) => {
+    const email = req.params.email;
+
+    try {
+      const contact = await contactRepository.findOne({ where: { email } });
+
+      if (!contact) {
+        return res.status(404).json({ error: `Contact with email ${email} not found` });
+      }
+
+      res.json({ contact });
+    } catch (error) {
+      console.error('Error fetching contact by email:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  });
+
+  router.get('/contactByPhoneNumber/:phoneNumber', async (req: Request, res: Response) => {
+    const phoneNumber = req.params.phoneNumber;
+  
+    try {
+      const contact = await contactRepository.findOne({ where:{ phoneNumber }});
+  
+      if (!contact) {
+        return res.status(404).json({ error: `Contact with phone number ${phoneNumber} not found` });
+      }
+  
+      res.json({ contact });
+    } catch (error) {
+      console.error('Error fetching contact by phone number:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  });
+  
+  router.get('/contact/:id', async (req: Request, res: Response) => {
+    const contactId = parseInt(req.params.id, 10);
+
+    try {
+      const contact = await contactRepository.findOneBy({ id: contactId });
+
+      if (!contact) {
+        return res.status(404).json({ error: `Contact with ID ${contactId} not found` });
+      }
+
+      res.json({ contact });
+    } catch (error) {
+      console.error('Error fetching contact by ID:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  });
+  
+
   return router;
 };
